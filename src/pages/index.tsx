de6704c3b9ba3d4973599343c404f2e9c380119b
David Gutman <dagutman@gmail.com>
2026-01-19 18:58:06 -0500

    Fix root redirect to use baseUrl path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
-  // Redirect root to docs index
-  return <Redirect to="/docs" />;
+  // Redirect to the baseUrl root where docs/index.md with slug: / is served
+  return <Redirect to="/bdsa-compute-blog/" />;
 }
